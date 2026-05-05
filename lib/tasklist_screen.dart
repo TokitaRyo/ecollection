31f0bde7a123c74ec3f937fcbb02b2e100b872a5
TokitaRyo <s2301088@sendai-nct.jp>
2026-05-05 14:30:31 +0900

    set buttom button of all

--- lib/tasklist_screen.dart
+++ lib/tasklist_screen.dart
@@ -11,6 +11,36 @@ class TaskListScreen extends StatelessWidget {
           style: TextStyle(color: Colors.white, fontSize: 24),
         ),
       ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(vertical: 12),
+        decoration: BoxDecoration(
+          color: Color(0xFF1A237E),
+          borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _navButton(context, Icons.emoji_events, '/ranking'),
+            _navButton(context, Icons.home, '/home'),
+            _navButton(context, Icons.list, '/tasks'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _navButton(BuildContext context, IconData icon, String route) {
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, route),
+      child: Container(
+        width: 64,
+        height: 64,
+        decoration: BoxDecoration(
+          color: Color(0xFFFF80AB),
+          shape: BoxShape.circle,
+        ),
+        child: Icon(icon, color: Color(0xFF1A237E), size: 30),
+      ),
     );
   }
 }
